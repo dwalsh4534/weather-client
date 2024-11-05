eca18edcdeb50ebbafb45380aa10f770122504d4
Daniel Walsh <dwalsh4534@gmail.com>
2024-11-05 05:49:45 -0600

    Reverted main branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [fetchedData, setFetchedData] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      const data = await axios.get(
-        "http://localhost:3001/"
-      );
-      setFetchedData(data);
-    };
-    getData();
-  }, []);
-
-
-  if (!fetchedData || fetchedData.length === 0) {
-    return <div>Loading...</div>; // or any placeholder
-}
-
-  console.log("data 1: ", fetchedData.data[0]);
-
-  const newData = fetchedData.data[0];
-
-  const {cityname, temperature, conditions, searchedat} = newData;
-
-  
   return (
     <div className="App">
-      <h1>test</h1>
-      <ul>
-      <li>{cityname}</li>
-      <li>{temperature}</li>
-      <li>{conditions}</li>
-      <li>{searchedat}</li>
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
