e13bae3ed8ba0bf0790091b2d0f07785147b9c31
Daniel Walsh <dwalsh4534@gmail.com>
2024-11-05 05:42:06 -0600

    Basic axios request to backend API example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(
+        "http://localhost:3001/"
+      );
+      setFetchedData(data);
+    };
+    getData();
+  }, []);
+
+
+  if (!fetchedData || fetchedData.length === 0) {
+    return <div>Loading...</div>; // or any placeholder
+}
+
+  console.log("data 1: ", fetchedData.data[0]);
+
+  const newData = fetchedData.data[0];
+
+  const {cityname, temperature, conditions, searchedat} = newData;
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
+      <ul>
+      <li>{cityname}</li>
+      <li>{temperature}</li>
+      <li>{conditions}</li>
+      <li>{searchedat}</li>
+      </ul>
     </div>
   );
 }
